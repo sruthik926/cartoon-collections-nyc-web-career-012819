ed56ec3fa2e67d35932d6ea6a233e590c3af051e
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 00:54:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(words)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
