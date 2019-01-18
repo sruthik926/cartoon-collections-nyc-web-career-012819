085081a71f8f60a46992bc2f3230ff459bc415d0
Sruthi Krishna <sruthik926@gmail.com>
2019-01-18 00:50:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls()
 end
 
 def find_the_cheese# code an argument here
